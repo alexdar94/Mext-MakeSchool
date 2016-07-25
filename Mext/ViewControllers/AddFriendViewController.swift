0a196eb7691cc2be2e5a35b71ad218597355bf64
Alex Lee <alexdar94@gmail.com>
2016-07-24 17:04:13 -0700

    Added add friend VC

---
+++ Mext/ViewControllers/AddFriendViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddFriendViewController.swift
+//  Mext
+//
+//  Created by Alex Lee on 24/07/2016.
+//  Copyright © 2016 Alex Lee. All rights reserved.
+//
+
+import UIKit
+
+class AddFriendViewController: UIViewController {
+
+}
