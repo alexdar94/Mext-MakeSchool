22d3d7f443b3cea02a6e43957b6b686488bf2fd3
Alex Lee <alexdar94@gmail.com>
2016-08-01 14:25:30 -0700

    Added segmented control to SoundChart page

---
+++ Mext/ViewControllers/SoundChartViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SoundChartViewController.swift
+//  Mext
+//
+//  Created by Alex Lee on 01/08/2016.
+//  Copyright © 2016 Alex Lee. All rights reserved.
+//
+
+import UIKit
+
+class SoundChartViewController: UIViewController {
+
+    @IBOutlet weak var mySoundsContainer: UIView!
+    @IBOutlet weak var trendingSoundsContainer: UIView!
+    
+    @IBAction func onSegmentChanged(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            UIView.animateWithDuration(0.5, animations: {
+                self.trendingSoundsContainer.alpha = 1
+                self.mySoundsContainer.alpha = 0
+            })
+            print("0")
+        } else {
+            UIView.animateWithDuration(0.5, animations: {
+                self.trendingSoundsContainer.alpha = 0
+                self.mySoundsContainer.alpha = 1
+            })
+            print("1")
+        }
+    }
+}
